623d67e58c9eb7ae55934415d8cb35c3089b4917
fuengfah01 <yasuriya_w@silpakorn.edu>
2026-03-18 18:30:44 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 # =========================
 # 🔑 CONFIG
 # =========================
-CHANNEL_ACCESS_TOKEN = "YOUR_TOKEN"
-CHANNEL_SECRET = "YOUR_SECRET"
+CHANNEL_ACCESS_TOKEN = "4daQ2JUnEe+vEmbDJhOmn48fWc7d/Kb6+iWXIm05H8ngOFqDPLyNpgdTO58cKvHyfcL/q/gytkIljJiMSjAQCvN5wmahGaLKoVocuepLo5tyQq7q33YfsPZPhxpO8kPOrpnECFRdZPB0JjHKaKaPOQdB04t89/1O/w1cDnyilFU="
+CHANNEL_SECRET = "a97e9e9977b3aac81ca9af33e59bde55"
 
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
